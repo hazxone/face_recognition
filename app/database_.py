ae45fefc032ba59f397d97a55a76aa9b991581d1
unknown <hazwan@gmail.com>
2018-09-11 12:09:03 +0800

    implement database

---
+++ app/database_.py
@@ -0,0 +1,26 @@
+import minidb
+import time
+
+def store_sqlite(company, uuid, endpoint, status):
+    db = minidb.Store('data/Apicall.db', debug=True)
+
+    class Call(minidb.Model):
+      company = str
+      uuid = str
+      time = str
+      endpoint = str
+      status = str
+
+    db.register(Call)
+
+    p = Call(company=company, uuid=uuid, time=str(time.asctime(time.localtime(time.time()))), endpoint = endpoint, status = status)
+
+    p.save(db)
+
+    # retrive = Call.load(db, (Call.c.company == 'sime'))
+    #
+    # for i in retrive:
+    #     print(i)
+
+    db.commit()
+    db.close()
--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ import random
 import pickle
 import os
 import numpy as np
+from data.L6SOsgE6HT import users
+import database_
 
 from flask_httpauth import HTTPBasicAuth
 import flask_monitoringdashboard as dashboard
@@ -22,9 +24,6 @@ dashboard.config.init_from(file='config.cfg')
 dashboard.bind(app)
 
 pickle_name = "blank.p"
-users = {
-        "medkad" : "mk101"
-}
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -41,7 +40,10 @@ def unauthorized():
     return make_response(jsonify({'Error':'Unauthorized Access'}), 403)
 
 @app.route('/compare', methods=['GET', 'POST'])
+@auth.login_required
 def compare():
+    company = auth.username()
+    unique_id = uuid.uuid4()
     # Check if a valid image file was uploaded
     if request.method == 'POST':
         if 'image_2' not in request.files:
@@ -54,33 +56,39 @@ def compare():
             return redirect(request.url)
 
         if image_1 and allowed_file(image_1.filename) and image_2 and allowed_file(image_2.filename):
-            return detect_faces_in_image(image_1, image_2)
+            return detect_faces_in_image(image_1, image_2, company, unique_id)
 
     # If no valid image file was uploaded, show the file upload form:
-    return '''
-    <!doctype html>
-    <title>Alphaface</title>
-    <h1>Upload pictures</h1>
-    <form method="POST" enctype="multipart/form-data">
-      <input type="file" name="file">
-      <input type="file" name="file2">
-      <input type="submit" value="Upload">
-    </form>
-    '''
+    result = {"Status" : "Data Handling Error"}
+    database_.store_sqlite(company, unique_id, "compare", result)
+    return jsonify(result)
+    #'''
+    # <!doctype html>
+    # <title>Alphaface</title>
+    # <h1>Upload pictures</h1>
+    # <form method="POST" enctype="multipart/form-data">
+    #   <input type="file" name="file">
+    #   <input type="file" name="file2">
+    #   <input type="submit" value="Upload">
+    # </form>
+    # '''
 
 @app.route('/register', methods=['GET', 'POST'])
 @auth.login_required
 def register_image():
     # Check if a valid image file was uploaded
+    company = auth.username()
+    unique_id = uuid.uuid4()
     if request.method == 'POST':
         if 'image_1' not in request.files:
             return redirect(request.url)
 
         image_1 = request.files['image_1']
         name = request.form['name']
-        company = auth.username()
-        pickle_name = company + ".p"
+        #company = auth.username()
+        pickle_name = "data/" +company + ".p"
         overwrite = True
+        #unique_id = uuid.uuid4()
 
         if overwrite == request.form['overwrite']:
             overwrite = False
@@ -111,23 +119,29 @@ def register_image():
         with open(pickle_name, 'wb') as f:
             pickle.dump(data, f)
 
-        result = { "Status" : "Successfully Add New Entry" }
+        result = { "Status" : "Successfully Add New Entry", "UID": unique_id}
+        database_.store_sqlite(company, unique_id, "register", result)
         return jsonify(result)
+    result = {"Status" : "Error", "Message" : "No Entry Added to Database"}
+    database_.store_sqlite(company, unique_id, "register", result)
+    return jsonify(result)
 
 
 @app.route('/verify', methods=['GET', 'POST'])
 @auth.login_required
 def verify_image():
+    company = auth.username()
+    unique_id = uuid.uuid4()
     # Check if a valid image file was uploaded
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'image_1' not in request.files:
             return redirect(request.url)
 
-        file = request.files['file']
-        company = auth.username()
-        pickle_name = company + ".p"
+        image_1 = request.files['image_1']
+        #company = auth.username()
+        pickle_name = "data/" + company + ".p"
 
-        if file.filename == '':
+        if image_1.filename == '':
             return redirect(request.url)
 
         if os.path.isfile(pickle_name):
@@ -139,13 +153,10 @@ def verify_image():
         face_names = list(data.keys())
         face_encodings = np.array(list(data.values()))
 
-        unknown_encoding = load_crop_encode(file)
+        unknown_encoding = load_crop_encode(image_1)
 
         result = face_recognition.compare_faces(face_encodings, unknown_encoding)
 
-        # recognize = False
-        # Print the result as a list of names with True/False
-        #names_with_result = list(zip(face_names, result))
         verify = (Counter(result))
         if (verify[True]) == 1:
         	for face_names, result in zip(face_names, result) :
@@ -164,8 +175,14 @@ def verify_image():
         result = {
             "Identity": json_name,
             "Status": recognize,
+            "UID": unique_id
         }
+        output = {"Identity": json_name, "Status": recognize,}
+        database_.store_sqlite(company, unique_id, "verify", output)
         return jsonify(result)
+    result = {"Status" : "Data Handling Error"}
+    database_.store_sqlite(company, unique_id, "verify", result)
+    return jsonify(result)
 
 def load_crop_encode(file):
     img_lce = face_recognition.load_image_file(file)
@@ -178,14 +195,14 @@ def load_crop_encode(file):
     encoding_lce = face_recognition.face_encodings(face_crop_lce)[0]
     return encoding_lce
 
-def detect_faces_in_image(file_stream_1, file_stream_2):
+def detect_faces_in_image(file_stream_1, file_stream_2, company, unique_id):
     # Load the uploaded image file
     face_1_enc = load_crop_encode(file_stream_1)
     face_2_enc = load_crop_encode(file_stream_2)
 
     epoch = time.time()
-    user_id = uuid.uuid4() # this could be incremental or even a uuid
-    unique_id = "%s_%d" % (user_id, epoch)
+    unique_id = uuid.uuid4() # this could be incremental or even a uuid
+    unique_id = "%s_%d" % (unique_id, epoch)
 
     #pil_image_1 = Image.fromarray(face_crop_1)
     #pil_image_2 = Image.fromarray(face_crop_2)
@@ -204,10 +221,11 @@ def detect_faces_in_image(file_stream_1, file_stream_2):
 
     # Return the result as json
     result = {
-        "Face_found_in_image": face_found,
         "Status": is_match,
         "UID": unique_id
     }
+    output = {"Status": is_match}
+    database_.store_sqlite(company, unique_id, "compare", output)
     return jsonify(result)
 
 if __name__ == "__main__":
